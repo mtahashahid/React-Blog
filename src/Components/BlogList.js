b6de77ce0b4dc231b1b20a5b6dadd61aa74d1291
Muhammad Taha <taha_shahid@outlook.com>
2021-06-22 16:13:22 +0500

    json data

--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -1,15 +1,15 @@
-const BlogList = ({blogs,title}) => {
-  
-    // const blogs = props.blogs;
-    // const title = props.title;
+const BlogList = ({ blogs, title }) => {
+  // const blogs = props.blogs;
+  // const title = props.title;
 
-    return (
+  return (
     <div className="blog-list">
-        <h2>{title}</h2>
+      <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.name}</h2>
+          <h2>{blog.title}</h2>
           <p>Written By {blog.author}</p>
+          {/* <button onClick={() => deleteBlog(blog.id)}>Delete Blog</button> */}
         </div>
       ))}
     </div>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,43 +1,75 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      name: "University Life",
-      body: "Lorem, ipsum dolor sit amet cons",
-      author: "Taha",
-      id: 1,
-    },
-    {
-      name: "College Life",
-      body: "Lorem, ipsum dolor sit amet cons",
-      author: "Bilal",
-      id: 2,
-    },
-    {
-      name: "Birthday Party",
-      body: "Lorem, ipsum dolor sit amet cons",
-      author: "Taha",
-      id: 3,
-    },
-    {
-      name: "Birthday Party",
-      body: "Lorem, ipsum dolor sit amet cons",
-      author: "Taha",
-      id: 3,
-    },
-    {
-      name: "Birthday Party",
-      body: "Lorem, ipsum dolor sit amet cons",
-      author: "Taha",
-      id: 3,
-    },
-  ]);
+  // const [blogs, setBlogs] = useState([
+  //   {
+  //     name: "University Life",
+  //     body: "Lorem, ipsum dolor sit amet cons",
+  //     author: "Taha",
+  //     id: 1,
+  //   },
+  //   {
+  //     name: "College Life",
+  //     body: "Lorem, ipsum dolor sit amet cons",
+  //     author: "Bilal",
+  //     id: 2,
+  //   },
+  //   {
+  //     name: "Birthday Party",
+  //     body: "Lorem, ipsum dolor sit amet cons",
+  //     author: "Taha",
+  //     id: 4,
+  //   },
+  //   {
+  //     name: "Birthday Party",
+  //     body: "Lorem, ipsum dolor sit amet cons",
+  //     author: "Taha",
+  //     id: 5,
+  //   },
+  //   {
+  //     name: "Birthday Party",
+  //     body: "Lorem, ipsum dolor sit amet cons",
+  //     author: "Taha",
+  //     id: 6,
+  //   },
+  // ]);
+
+  const [isPending, setisPending] = useState(true);
+
+  const [error, setError] = useState(null);
+  const [blogs, setBlogs] = useState(null);
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch teh data from that resource");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setisPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setisPending(false);
+        setError(err.message);
+      });
+  }, []);
+
+  // const deleteBlog = (id) =>{
+  //   const newBlogs = blogs.filter(blog => blog.id !== id)
+  //   setBlogs(newBlogs);
+  // }
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs"/>
-      <BlogList blogs={blogs.filter((blog)=>blog.author === "Taha")} title="All Blogs"/>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {/* {blogs && <BlogList blogs={blogs} title="All Blogs" deleteBlog={deleteBlog}/>} */}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {/* <BlogList blogs={blogs.filter((blog)=>blog.author === "Taha")} title="All Blogs" /> */}
     </div>
   );
 };
